5c0bb311a815b051344e118f6b12740ba4c31e68
unknown <maxime.baron2002@gmail.com>
2022-12-22 15:48:44 +0100

    :construction: back done vue WIP3

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,5 @@
 const checkbox = document.querySelector('#completeSubscription')
-
+console.log("tesest")
 checkbox.addEventListener('change',(e)=>{
     console.log(e)
 })
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import "assets\\js\\form.js"
+import 'assets/js/form.js'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
