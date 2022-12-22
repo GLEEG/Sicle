e2607ab9348d075da1d8ac823f8697b060bde770
ayman-benammour <benammourayman@gmail.com>
2022-12-22 11:01:04 +0100

    :sparkles: Add favicon and change title

--- index.php
+++ index.php
@@ -292,10 +292,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>webpack php</title>
+    <title>Sicle - L'application mobile ludique et motivante</title>
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="./assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="./assets/favicon/safari-pinned-tab.svg" color="#272f38">
+    <link rel="shortcut icon" href="./assets/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="./assets/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <!-- CSS -->
     <link rel="stylesheet" href="./assets/styles/reset.css">
     <link rel="stylesheet" href="./assets/styles/style.css">
-    <!-- <link rel="stylesheet" href="./assets/styles/cursor.css"> -->
     <link rel="stylesheet" href="https://unpkg.com/kursor/dist/kursor.css">
 </head> 
 <body>
